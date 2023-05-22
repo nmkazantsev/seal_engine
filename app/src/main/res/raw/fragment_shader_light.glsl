3bf286346bdf0417b4990dc9f228f2592dd0e46d
nmkazantsev <nmkazantsev@gmail.com>
2023-05-22 19:49:55 +0300

    made normal data send

--- app/src/main/res/raw/fragment_shader_light.glsl
+++ app/src/main/res/raw/fragment_shader_light.glsl
@@ -4,36 +4,46 @@ out vec4 FragColor;
 uniform sampler2D textureSamp;
 uniform sampler2D normalMap;
 
+struct DirectionalLight{
+    vec3 lightPos;
+    float specular;
+    float diffuse;
+    float ambinient;
+};
+
+uniform DirectionalLight dirLights [10];
+
 in struct Data{
-    vec3 normal;
     mat4 model2;
+    vec3 normal;
     vec3 FragPos;
     vec2 TexCoord;
-    vec3 TangentLightPos;
     vec3 TangentViewPos;
     vec3 TangentFragPos;
+    int dirLightNum;
 } data;
+in vec3 lightPos[10];
 
 
 void main()
 {
+    vec3 viewDir = normalize(data.TangentViewPos - data.TangentFragPos);
     vec3 norm = texture(normalMap, data.TexCoord).rgb;
     norm = normalize(norm * 2.0 - 1.0);
-    vec3 color =texture(textureSamp, data.TexCoord).rgb;
-    // ambient
-    vec3 ambient = 0.35 * color;
-    // diffuse
-    vec3 lightDir = normalize(data.TangentLightPos - data.TangentFragPos);
-    float diff = max(dot(lightDir, norm), 0.0);
-    vec3 diffuse = diff * color;
-    // specular
-    vec3 viewDir = normalize(data.TangentViewPos - data.TangentFragPos);
-
-
-    vec3 reflectDir = reflect(-lightDir, norm);
-    vec3 halfwayDir = normalize(lightDir + viewDir);
-    float spec = pow(max(dot(norm, halfwayDir), 0.0), 32.0);
-    // spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
-    vec3 specular = vec3(0.3) * spec;// assuming bright white light color
-    FragColor = vec4(ambient + diffuse + specular, 1.0);// vec4(norm,1.0);//textureColor*diff*vec4(lightColor, 1.0);
+    vec3 color = texture(textureSamp, data.TexCoord).rgb;
+    for (int i=0;i<data.dirLightNum;i++){
+        // ambient
+        vec3 ambient = dirLights[i].ambinient * color;
+        // diffuse
+        vec3 lightDir = normalize(lightPos[i] - data.TangentFragPos);
+        float diff = max(dot(lightDir, norm), 0.0);
+        vec3 diffuse = diff * color;
+        // specular
+        vec3 reflectDir = reflect(-lightDir, norm);
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        float spec = pow(max(dot(norm, halfwayDir), 0.0), 32.0);
+        // spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0); //phong
+        vec3 specular = vec3(0.3) * spec;// assuming bright white light color
+        FragColor += vec4(ambient + diffuse + specular, 1.0);// vec4(norm,1.0);//textureColor*diff*vec4(lightColor, 1.0);
+    }
 }
--- app/src/main/res/raw/vertex_shader_light.glsl
+++ app/src/main/res/raw/vertex_shader_light.glsl
@@ -6,25 +6,37 @@ layout (location = 2) in vec3 normalVec;
 layout (location = 3) in vec3 aT;//abs tangent
 layout (location = 4) in vec3 aB;//absolute bitangent
 
+struct DirectionalLight{
+    vec3 lightPos;
+    float specular;
+    float diffuse;
+    float ambinient;
+};
+
+
 out struct Data{
-    vec3 normal;
     mat4 model2;
+    vec3 normal;
     vec3 FragPos;
     vec2 TexCoord;
-    vec3 TangentLightPos;
     vec3 TangentViewPos;
     vec3 TangentFragPos;
+    int dirLightNum;
 } data;
+out vec3 lightPos[10];
 
+
+uniform int dirLightNum;
+uniform DirectionalLight dirLights [10];
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform vec3 viewPos;
 
-
-vec3 lightPos =  vec3(0.0, 3.0, 0.0);
+//vec3 lightPos =  vec3(0.0, 3.0, 0.0);
 void main()
 {
+    data.dirLightNum=dirLightNum;
     gl_Position = projection * view * model * vec4(aPos, 1.0f);
     data.FragPos = (vec4(aPos, 1.0f)*transpose(model)).xyz;
     data.TexCoord = vec2(aTexCoord.x, aTexCoord.y);
@@ -34,7 +46,9 @@ void main()
     vec3 B = normalize(vec3(model * vec4(aB, 0.0)));
     vec3 N = normalize(vec3(model * vec4(data.normal, 0.0)));
     mat3 TBN = transpose(mat3(T, B, N));
-    data.TangentLightPos = TBN * lightPos;
+    for(int i=0;i<dirLightNum;i++){
+        lightPos[i] = TBN * dirLights[i].lightPos;
+    }
     data.TangentViewPos  = TBN * viewPos;
     data.TangentFragPos  = TBN * vec3(model * vec4(aPos, 1.0));
 }
