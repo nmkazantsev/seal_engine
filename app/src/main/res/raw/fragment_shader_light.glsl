06456fbbdafc9fbdd47920c8dd277e9f6c7b4fae
nmkazantsev <nmkazantsev@gmail.com>
2023-05-22 18:40:12 +0300

    struct shaders

--- app/src/main/res/raw/fragment_shader_light.glsl
+++ app/src/main/res/raw/fragment_shader_light.glsl
@@ -1,37 +1,39 @@
 #version 300 es
 precision mediump float;
 out vec4 FragColor;
-in vec2 TexCoord;
 uniform sampler2D textureSamp;
 uniform sampler2D normalMap;
 
-in vec3 normal;
-in mat4 model2;
-in vec3 FragPos;
-in vec3 TangentLightPos;
-in vec3 TangentViewPos;
-in vec3 TangentFragPos;
+in struct Data{
+    vec3 normal;
+    mat4 model2;
+    vec3 FragPos;
+    vec2 TexCoord;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} data;
 
 
 void main()
 {
-    vec3 norm = texture(normalMap, TexCoord).rgb;
+    vec3 norm = texture(normalMap, data.TexCoord).rgb;
     norm = normalize(norm * 2.0 - 1.0);
-    vec3 color =texture(textureSamp, TexCoord).rgb;
+    vec3 color =texture(textureSamp, data.TexCoord).rgb;
     // ambient
     vec3 ambient = 0.35 * color;
     // diffuse
-    vec3 lightDir = normalize(TangentLightPos - TangentFragPos);
+    vec3 lightDir = normalize(data.TangentLightPos - data.TangentFragPos);
     float diff = max(dot(lightDir, norm), 0.0);
     vec3 diffuse = diff * color;
     // specular
-    vec3 viewDir = normalize(TangentViewPos - TangentFragPos);
+    vec3 viewDir = normalize(data.TangentViewPos - data.TangentFragPos);
 
 
     vec3 reflectDir = reflect(-lightDir, norm);
     vec3 halfwayDir = normalize(lightDir + viewDir);
     float spec = pow(max(dot(norm, halfwayDir), 0.0), 32.0);
-   // spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
+    // spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
     vec3 specular = vec3(0.3) * spec;// assuming bright white light color
     FragColor = vec4(ambient + diffuse + specular, 1.0);// vec4(norm,1.0);//textureColor*diff*vec4(lightColor, 1.0);
 }
--- app/src/main/res/raw/vertex_shader_light.glsl
+++ app/src/main/res/raw/vertex_shader_light.glsl
@@ -6,35 +6,35 @@ layout (location = 2) in vec3 normalVec;
 layout (location = 3) in vec3 aT;//abs tangent
 layout (location = 4) in vec3 aB;//absolute bitangent
 
-out vec2 TexCoord;
-out vec3 normal;
-out mat4 model2;
-out vec3 FragPos;
-
-out vec3 TangentLightPos;
-out vec3 TangentViewPos;
-out vec3 TangentFragPos;
-
+out struct Data{
+    vec3 normal;
+    mat4 model2;
+    vec3 FragPos;
+    vec2 TexCoord;
+    vec3 TangentLightPos;
+    vec3 TangentViewPos;
+    vec3 TangentFragPos;
+} data;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform vec3 viewPos;
-//mat4 buff;
+
 
 vec3 lightPos =  vec3(0.0, 3.0, 0.0);
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0f);
-    FragPos = (vec4(aPos, 1.0f)*transpose(model)).xyz;
-    TexCoord = vec2(aTexCoord.x, aTexCoord.y);
-    normal=normalize(normalVec);
-    model2=model;
+    data.FragPos = (vec4(aPos, 1.0f)*transpose(model)).xyz;
+    data.TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+    data.normal=normalize(normalVec);
+    data.model2=model;
     vec3 T = normalize(vec3(model * vec4(aT, 0.0)));
     vec3 B = normalize(vec3(model * vec4(aB, 0.0)));
-    vec3 N = normalize(vec3(model * vec4(normal, 0.0)));
+    vec3 N = normalize(vec3(model * vec4(data.normal, 0.0)));
     mat3 TBN = transpose(mat3(T, B, N));
-    TangentLightPos = TBN * lightPos;
-    TangentViewPos  = TBN * viewPos;
-    TangentFragPos  = TBN * vec3(model * vec4(aPos, 1.0));
+    data.TangentLightPos = TBN * lightPos;
+    data.TangentViewPos  = TBN * viewPos;
+    data.TangentFragPos  = TBN * vec3(model * vec4(aPos, 1.0));
 }
