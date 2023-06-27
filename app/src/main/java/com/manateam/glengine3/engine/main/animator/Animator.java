f52f6a0e885a49fedf8137c2eaaab29489d29f68
AntonMiklushov <103569137+AntonMiklushov@users.noreply.github.com>
2023-06-27 12:00:11 +0300

    refactored animations, wrong gradle version

--- app/src/main/java/com/manateam/glengine3/engine/main/animator/Animator.java
+++ app/src/main/java/com/manateam/glengine3/engine/main/animator/Animator.java
@@ -3,55 +3,45 @@ package com.manateam.glengine3.engine.main.animator;
 import static com.manateam.glengine3.utils.Utils.contactArray;
 import static com.manateam.glengine3.utils.Utils.millis;
 
+import android.graphics.drawable.AnimatedImageDrawable;
+
 import com.manateam.glengine3.engine.main.engine_object.EnObject;
 
+import java.util.HashMap;
 import java.util.function.Function;
 
 public class Animator {
-    private static Animator animatorsQueue[];
-    private boolean active;
-    private Animation animQueue[];
-    private EnObject target;
-    public Animator (EnObject obj) {
-        if (animatorsQueue == null) animatorsQueue = new Animator[]{this};
-        else {
-            Animator b[] = new Animator[animatorsQueue.length + 1];
-            for (int i = 0; i < animatorsQueue.length; i++) animatorsQueue[i] = b[i];
-            b[animatorsQueue.length] = this;
-            animatorsQueue = b;
+    private static HashMap<Long, Animation> animQueue;
+    private static long idCounter = 0;
+
+    private static long listAnimation(Animation animation) {
+        if (animQueue == null) {
+            animQueue = new HashMap<Long, Animation>();
+            animQueue.put(idCounter, animation);
         }
-        active = true;
-        animQueue = new Animation[]{};
-        target = obj;
+        animQueue.put(idCounter, animation);
+        idCounter++;
+        return idCounter - 1;
     }
 
-    public boolean getActive() {
-        return active;
+    private static void deleteAnimation(long id) {
+        animQueue.remove(id);
     }
-
-    public void activate() {
-        active = true;
-    }
-
-    public void inactivate() {
-        active = false;
-    }
-
-    // todo make it be applied on the EnObject
-    public void goThroughAnimQueue() {
-        for (Animation a: this.animQueue) a.getAnimMatrix(target.getMatrix());
-    }
+    public static void animate(EnObject target) {}
 
     public static class Animation {
 
-        private Function<float[], float[]> tf;
-        private Function<float[], Object> vf;
-        private float[] args;
-        private float duration;
+        private boolean state; // active/inactive
+        private EnObject target;
+        private Function<float[], float[]> tf; // transmission function
+        private Function<float[], Object> vf; // velocity function
+        private float[] args; // additional arguments
+        private float duration; // total duration
         // velocity function argument
-        private float vfa;
-        private long startTiming;
-        // tf - transformation function, vf - velocity function
+        private float vfa; // velocity function argument
+        private long startTiming; // global start timing in millis
+        private float buffer;
+        private long id;
         public Animation(Function tf, float[] args, Function vf, float duration, float vfa) {
             startTiming = millis();
             this.tf = tf;
@@ -59,19 +49,24 @@ public class Animator {
             this.args = args;
             this.duration = duration;
             this.vfa = vfa;
+            this.buffer = 0;
+            this.id = listAnimation(this);
         }
 
         @Override
         public void finalize() {
+            deleteAnimation(id);
         }
 
         // todo find out is it possible to make functions take more than one argument
         public float[] getAnimMatrix(float[] matrix) {
-            float t = (millis() - startTiming) / duration;
-            float dt = t - (float) vf.apply(new float[]{t, vfa});
-            if (t >= 1) finalize();
+            float gt = (millis() - startTiming) / duration; // global timing (linear from 0 to 1)
+            float t = (float) vf.apply(new float[]{gt, vfa}); // velocity function output for gt
+            float dt = t - buffer; // difference in current and previous vf output (shift delta)
+            buffer = t;
+            if (gt >= 1) finalize(); // completion
             float[] params = new float[16 + args.length];
-            float a[] = contactArray(args, new float[]{dt});
+            float[] a = contactArray(args, new float[]{dt});
             return tf.apply(contactArray(matrix, a));
         }
     }
--- app/src/main/java/com/manateam/glengine3/engine/main/engine_object/EnObject.java
+++ app/src/main/java/com/manateam/glengine3/engine/main/engine_object/EnObject.java
@@ -1,9 +1,14 @@
 package com.manateam.glengine3.engine.main.engine_object;
 
+import com.manateam.glengine3.engine.main.verticles.Shape;
+
 // todo make it work
 public class EnObject {
+    private Shape shape;
     private float[] drawMatrix;
     public float[] getMatrix() {
         return this.drawMatrix;
     }
+
+    EnObject() {}
 }
