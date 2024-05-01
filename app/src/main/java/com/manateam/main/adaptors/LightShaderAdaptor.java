422a15726d48706c8b78b951d762e282218bb369
nikita <nmkazantsev@gmail.com>
2024-05-01 23:05:10 +0300

    seems to make it work

--- app/src/main/java/com/manateam/main/adaptors/LightShaderAdaptor.java
+++ app/src/main/java/com/manateam/main/adaptors/LightShaderAdaptor.java
@@ -213,18 +213,18 @@ public class LightShaderAdaptor extends Adaptor {
 
         GLES30.glUniform1i(glGetUniformLocation(programId, "pLightNumber"), 1);
 */
-        GLES30.glUniform3f(glGetUniformLocation(programId, "sLights[" + 0 + "].direction"), 1, -1, -3);
+        GLES30.glUniform3f(glGetUniformLocation(programId, "sLights[" + 0 + "].direction"), 1, 1, -1.5f);
         GLES30.glUniform3f(glGetUniformLocation(programId, "sLights[" + 0 + "].color"), 0.5f, 0.5f, 0.5f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].diffuse"), 0.7f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].specular"), 0.1f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].ambient"), 0.1f);
 
 
-        GLES30.glUniform3f(glGetUniformLocation(programId, "sLights[" + 0 + "].position"), 1, 1, 3);
+        GLES30.glUniform3f(glGetUniformLocation(programId, "sLights[" + 0 + "].position"), 1, 1, 1.5f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].constant"), 0.1f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].linear"), 0.1f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].quadratic"), 0.1f);
-        GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].outerCutOff"), 0.1f);
+        GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].outerCutOff"), 0.2f);
         GLES30.glUniform1f(glGetUniformLocation(programId, "sLights[" + 0 + "].cutOff"), 0.1f);
 
 
