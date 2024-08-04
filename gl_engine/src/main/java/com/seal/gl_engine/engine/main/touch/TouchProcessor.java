e331157b991cafd6f045965490c6b0edc5de98ea
nmkazantsev <nmkazantsev@gmail.com>
2024-08-04 17:21:56 +0300

    add page change processing

--- gl_engine/src/main/java/com/seal/gl_engine/engine/main/touch/TouchProcessor.java
+++ gl_engine/src/main/java/com/seal/gl_engine/engine/main/touch/TouchProcessor.java
@@ -4,8 +4,12 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.seal.gl_engine.GamePageInterface;
+import com.seal.gl_engine.OpenGLRenderer;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
 
@@ -15,6 +19,7 @@ import java.util.function.Function;
  */
 public class TouchProcessor {
     private Integer touchId = 0;
+    private String creatorClassName = null;
     private static final HashMap<Integer, TouchProcessor> activeProcessors = new HashMap<>();
     private static final List<TouchProcessor> allProcessors = new ArrayList<>();
     private Function<MotionEvent, Boolean> checkHitboxCallback;
@@ -29,11 +34,17 @@ public class TouchProcessor {
      * @param touchStartedCallback called when this object captures a new touch.
      * @param touchMovedCallback   called when captured touch moves.
      * @param touchEndedCallback   called when captured touch ends.
+     * @param creatorPage          not null creator page object.
      */
     public TouchProcessor(Function<MotionEvent, Boolean> checkHitboxCallback,
                           Function<TouchPoint, Void> touchStartedCallback,
                           Function<TouchPoint, Void> touchMovedCallback,
-                          Function<Void, Void> touchEndedCallback) {
+                          Function<Void, Void> touchEndedCallback, GamePageInterface creatorPage) {
+        creatorClassName = (String) creatorPage.getClass().getName();
+        this.checkHitboxCallback = checkHitboxCallback;
+        this.touchStartedCallback = touchStartedCallback;
+        this.touchMovedCallback = touchMovedCallback;
+        this.touchEndedCallback = touchEndedCallback;
         allProcessors.add(this);
     }
 
@@ -53,6 +64,11 @@ public class TouchProcessor {
 
     private boolean checkHitbox(MotionEvent event) {
         return checkHitboxCallback.apply(event);
+    }
+
+    private void delete() {
+        allProcessors.remove(this);
+        activeProcessors.remove(touchId);
     }
 
     //**********STATIC METHODS********************
@@ -90,6 +106,20 @@ public class TouchProcessor {
 
     private static void touchEnded(MotionEvent event) {
         activeProcessors.get(event.getPointerId(event.getActionIndex())).terminate(event);
+    }
+
+    protected static void onPageChange() {
+        Iterator<TouchProcessor> iterator = allProcessors.iterator();
+        while (iterator.hasNext()) {
+            TouchProcessor e = iterator.next();
+            if (e.creatorClassName != null) {
+                if (!e.creatorClassName.equals(OpenGLRenderer.getPageClassName())) {
+                    e.terminate();
+                    e.delete();
+                    iterator.remove();
+                }
+            }
+        }
     }
 }
 
