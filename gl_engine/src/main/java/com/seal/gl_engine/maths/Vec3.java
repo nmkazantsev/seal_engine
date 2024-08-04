121631bd7d1110981c79a95bb93caae851187f8c
nmkazantsev <nmkazantsev@gmail.com>
2024-08-04 15:15:05 +0300

    add rotate vecr

--- gl_engine/src/main/java/com/seal/gl_engine/maths/Vec3.java
+++ gl_engine/src/main/java/com/seal/gl_engine/maths/Vec3.java
@@ -1,7 +1,10 @@
 package com.seal.gl_engine.maths;
 
+import static com.seal.gl_engine.engine.config.MainConfigurationFunctions.resetTranslateMatrix;
 import static java.lang.Math.acos;
 import static java.lang.Math.tan;
+
+import android.opengl.Matrix;
 
 import com.seal.gl_engine.utils.Utils;
 
@@ -121,5 +124,40 @@ public class Vec3 {
         Vec3 b = (new Vec3(v)).add(c.mul((float) tan(alpha)));
         b.normalize();
         return b.mul(v.length());
+    }
+
+    /**
+     rotates vector around axis for a specified angle
+     @param vec  source vector
+     @param axis  axis, around which to rotate
+     @param a  angle in degrees
+     @return  new Vec3, equal to rotated vector
+     */
+    public static Vec3 rotateVec3(Vec3 vec, Vec3 axis, float a) {
+        //create empty translate matrix
+        float matrix[];
+        matrix = resetTranslateMatrix(new float[16]);
+        Matrix.rotateM(matrix, 0, a, axis.x, axis.y, axis.z);
+        float resultVec[] = new float[4];
+        Matrix.multiplyMV(resultVec, 0, matrix, 0, new float[]{vec.x, vec.y, vec.z, 0}, 0);
+        return new Vec3(resultVec[0], resultVec[1], resultVec[2]);
+    }
+
+    /**
+     rotates vector around axis for a specified angle
+     @param axis  axis, around which to rotate
+     @param a  angle in degrees
+     */
+    public void rotateVec3(Vec3 axis, float a) {
+        //create empty translate matrix
+        float matrix[];
+        matrix = resetTranslateMatrix(new float[16]);
+        Matrix.rotateM(matrix, 0, a, axis.x, axis.y, axis.z);
+        float resultVec[] = new float[4];
+        Matrix.multiplyMV(resultVec, 0, matrix, 0, new float[]{this.x, this.y, this.z, 0}, 0);
+        //return new Vec3(resultVec[0], resultVec[1], resultVec[2]);
+        this.x = resultVec[0];
+        this.y = resultVec[1];
+        this.z = resultVec[2];
     }
 }
