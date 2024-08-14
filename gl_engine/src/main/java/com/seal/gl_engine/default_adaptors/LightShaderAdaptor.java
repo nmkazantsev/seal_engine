c604f6e05a752cab3b332db710288253ee95f4a5
nikita <nmkazantsev@gmail.com>
2024-08-14 10:01:28 +0300

    add default adaptors

--- gl_engine/src/main/java/com/seal/gl_engine/default_adaptors/LightShaderAdaptor.java
+++ gl_engine/src/main/java/com/seal/gl_engine/default_adaptors/LightShaderAdaptor.java
@@ -1,4 +1,4 @@
-package com.manateam.main.adaptors;
+package com.seal.gl_engine.default_adaptors;
 
 import static android.opengl.GLES20.GL_ARRAY_BUFFER;
 import static android.opengl.GLES20.GL_FLOAT;
@@ -20,28 +20,32 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
-public class MainShaderAdaptor extends Adaptor {
+public class LightShaderAdaptor extends Adaptor {
 
+    private int normalMapEnableLocation;
     private int aPositionLocation;
     private int aTextureLocation;
     private int uTextureUnitLocation;
     private int projectionMatrixLoation;
     private int viewMatrixLocation;
     private int modelMtrixLocation;
-    private int normalLocation;
+    private int normalLocation, normalMapLocation;
+    private int tangetntLocation, bitangentLocation, cameraPosLocation;
 
     private final static int POSITION_COUNT = 3;
     private static final int TEXTURE_COUNT = 2;
     private static final int NORMAL_COUNT = 3;
+    private static final int TANGENT_VEC = 3;
+    private static final int BITANGENT_VEC = 3;
     private static final int STRIDE = (POSITION_COUNT
-            + TEXTURE_COUNT + NORMAL_COUNT) * 4;
+            + TEXTURE_COUNT + NORMAL_COUNT + TANGENT_VEC + BITANGENT_VEC) * 4;
 
     @Override
     public int bindData(Face[] faces) {
-        float[] vertices = new float[faces.length * faces[0].verticesNumber()];
+        float[] vertices = new float[faces.length * (faces[0].verticesNumberTangentSpace())];
         int vertexesNumber = 0;
         for (int i = 0; i < faces.length; i++) {
-            System.arraycopy(faces[i].getArrayRepresentation(), 0, vertices, i * faces[i].verticesNumber(), faces[i].verticesNumber());
+            System.arraycopy(faces[i].getArrayRepresentationTangentSpace(), 0, vertices, i * (faces[i].verticesNumberTangentSpace()), faces[i].verticesNumberTangentSpace());
             vertexesNumber++;
         }
         FloatBuffer vertexData = ByteBuffer
@@ -65,6 +69,17 @@ public class MainShaderAdaptor extends Adaptor {
         glVertexAttribPointer(normalLocation, NORMAL_COUNT, GL_FLOAT,
                 false, STRIDE, vertexData);
         glEnableVertexAttribArray(normalLocation);
+
+        vertexData.position(POSITION_COUNT + TEXTURE_COUNT + NORMAL_COUNT);
+        glVertexAttribPointer(tangetntLocation, TANGENT_VEC, GL_FLOAT,
+                false, STRIDE, vertexData);
+        glEnableVertexAttribArray(tangetntLocation);
+
+        vertexData.position(POSITION_COUNT + TEXTURE_COUNT + NORMAL_COUNT + TANGENT_VEC);
+        glVertexAttribPointer(bitangentLocation, BITANGENT_VEC, GL_FLOAT,
+                false, STRIDE, vertexData);
+        glEnableVertexAttribArray(bitangentLocation);
+
         return vertexesNumber;
     }
 
@@ -97,7 +112,7 @@ public class MainShaderAdaptor extends Adaptor {
         loadDataToBuffer(vertices, 0, vertexBuffer);
 
         //set up uv
-        vertices = new float[faces.length * 2 * 3];//3 because 3angle
+        vertices = new float[faces.length * faces[0].textureCoordinates.length * 3];//3 because 3angle
         for (int i = 0; i < faces.length; i++) {
             vertices[i * 6] = faces[i].textureCoordinates[0].x;
             vertices[i * 6 + 1] = 1 - faces[i].textureCoordinates[0].y;
@@ -109,25 +124,56 @@ public class MainShaderAdaptor extends Adaptor {
         loadDataToBuffer(vertices, 1, vertexBuffer);
 
         //set up normals
-        vertices = new float[faces.length * 3];//3 because 3 coords in normal
+        vertices = new float[faces.length * 3 * 3];//3 because 3 coords in normal
         for (int i = 0; i < faces.length; i++) {
-            vertices[i * faces[i].vertices.length] = faces[i].normal.x;
-            vertices[i * faces[i].vertices.length + 1] = faces[i].normal.y;
-            vertices[i * faces[i].vertices.length + 2] = faces[i].normal.z;
-
+            for (int g = 0; g < 3; g++) {
+                vertices[i * 9 + g * 3] = faces[i].normal.x;
+                vertices[i * 9 + g * 3 + 1] = faces[i].normal.y;
+                vertices[i * 9 + g * 3 + 2] = faces[i].normal.z;
+            }
         }
         loadDataToBuffer(vertices, 2, vertexBuffer);
+
+        //set up tangent
+        vertices = new float[faces.length * 3 * 3];//3 because 3 coords in normal
+        for (int i = 0; i < faces.length; i++) {
+            for (int g = 0; g < 3; g++) {
+                vertices[i * 9 + g * 3] = faces[i].tangent.x;
+                vertices[i * 9 + g * 3 + 1] = faces[i].tangent.y;
+                vertices[i * 9 + g * 3 + 2] = faces[i].tangent.z;
+            }
+
+        }
+        loadDataToBuffer(vertices, 3, vertexBuffer);
+
+        //set up bi tangent
+        vertices = new float[faces.length * 3 * 3];//3 because 3 coords in normal
+        for (int i = 0; i < faces.length; i++) {
+            for (int g = 0; g < 3; g++) {
+                vertices[i * 9 + g * 3] = faces[i].bitangent.x;
+                vertices[i * 9 + g * 3 + 1] = faces[i].bitangent.y;
+                vertices[i * 9 + g * 3 + 2] = faces[i].bitangent.z;
+            }
+        }
+        loadDataToBuffer(vertices, 4, vertexBuffer);
 
         vertexBuffer.bindVao();
         glEnableVertexAttribArray(aPositionLocation);
         glEnableVertexAttribArray(aTextureLocation);
-        // glEnableVertexAttribArray(normalLocation);
+        glEnableVertexAttribArray(normalLocation);
+        glEnableVertexAttribArray(tangetntLocation);
+        glEnableVertexAttribArray(bitangentLocation);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.getVboAdress(0));
         glVertexAttribPointer(aPositionLocation, 3, GL_FLOAT, false, 0, 0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.getVboAdress(1));
         glVertexAttribPointer(aTextureLocation, 2, GL_FLOAT, false, 0, 0);
-        // glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.getVboAdress(2));
-        //glVertexAttribPointer(normalLocation, 3, GL_FLOAT, false, 0, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.getVboAdress(2));
+        glVertexAttribPointer(normalLocation, 3, GL_FLOAT, false, 0, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.getVboAdress(3));
+        glVertexAttribPointer(tangetntLocation, 3, GL_FLOAT, false, 0, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.getVboAdress(4));
+        glVertexAttribPointer(bitangentLocation, 3, GL_FLOAT, false, 0, 0);
+
         vertexBuffer.bindDefaultVbo();//vertex coords
         vertexBuffer.bindDefaultVao();
         return 0;
@@ -137,11 +183,16 @@ public class MainShaderAdaptor extends Adaptor {
     public void updateLocations() {
         aPositionLocation = glGetAttribLocation(programId, "aPos");
         aTextureLocation = glGetAttribLocation(programId, "aTexCoord");
-        normalLocation = glGetAttribLocation(programId, "normal");
+        normalLocation = glGetAttribLocation(programId, "normalVec");
+        tangetntLocation = glGetAttribLocation(programId, "aT");
+        bitangentLocation = glGetAttribLocation(programId, "aB");
         uTextureUnitLocation = glGetUniformLocation(programId, "u_TextureUnit");
+        normalMapLocation = glGetUniformLocation(programId, "normalMap");
         projectionMatrixLoation = GLES30.glGetUniformLocation(programId, "projection");
         viewMatrixLocation = GLES30.glGetUniformLocation(programId, "view");
         modelMtrixLocation = GLES30.glGetUniformLocation(programId, "model");
+        cameraPosLocation = GLES30.glGetUniformLocation(programId, "viewPos");
+        normalMapEnableLocation = GLES30.glGetUniformLocation(programId, "normalMapEnable");
     }
 
     @Override
@@ -166,16 +217,16 @@ public class MainShaderAdaptor extends Adaptor {
 
     @Override
     public int getNormalTextureLocation() {
-        return -1;
+        return normalMapLocation;
     }
 
     @Override
     public int getNormalMapEnableLocation() {
-        return -1;
+        return normalMapEnableLocation;
     }
 
     @Override
     public int getCameraPosLlocation() {
-        return -1;
+        return cameraPosLocation;
     }
 }
